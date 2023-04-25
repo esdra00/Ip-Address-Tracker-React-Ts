8bd9b7df16ee8b397cf6fb4f97d741c1dccc1856
Esdra Bergamasco <esdrabergamasco@gmail.com>
2023-04-25 16:00:52 +0200

    Changed base name and deployed to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-	base: "/ip-address-tracker-react-ts-test/",
+	plugins: [react()],
+	base: "/Ip-Address-Tracker-React-Ts/",
 });
